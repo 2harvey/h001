de8bd767219f6b57c3a63578b3210ec5cb258904
2harvey <harvey4x@foxmail.com>
2017-12-21 12:11:51 +0800

    config中心

---
+++ center-config/src/main/java/com/github/h001/config/ConfigServerBootstrap.java
@@ -0,0 +1,17 @@
+package com.github.h001.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/**
+ * Created by 2harvey on 2017年12月.
+ */
+@EnableAutoConfiguration
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerBootstrap.class, args);
+    }
+}
--- center-register/src/main/java/com/github/h001/center/CenterRegisterBootstrap.java
+++ center-register/src/main/java/com/github/h001/center/CenterRegisterBootstrap.java
@@ -12,8 +12,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
-public class CenterBootstrap {
+public class CenterRegisterBootstrap {
     public static void main(String[] args) {
-        SpringApplication.run(CenterBootstrap.class, args);
+        SpringApplication.run(CenterRegisterBootstrap.class, args);
     }
 }
